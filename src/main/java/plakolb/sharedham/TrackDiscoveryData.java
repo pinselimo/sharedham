e5b56ba22d65f35cef748b0bdeef69eb562a7256
Simon Plakolb <s.plakolb@gmail.com>
2020-03-07 15:50:25 +0100

    Added some data management code

---
+++ src/main/java/plakolb/sharedham/TrackDiscoveryData.java
@@ -0,0 +1,18 @@
+package plakolb.sharedham;
+
+/**
+ * Created by pinselimo on 04/04/16.
+ */
+public class TrackDiscoveryData
+{
+	public final String artist;
+	public final String album;
+	public final String trackname;
+
+	public TrackDiscoveryData(String artist, String album, String trackname)
+	{
+		this.artist = artist;
+		this.album = album;
+		this.trackname = trackname;
+	}
+}
